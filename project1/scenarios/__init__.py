410eee503ae1377a917ab0473421dada7746c1d3
eabartlett <ebartlett9@gmail.com>
2013-10-07 21:34:24 -0700

    delete extra

--- project1/scenarios/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Contains various network topologies and scenarios.
-See help of individual scenarios for more info.
-"""
--- project1/sim/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-The network simulator.
-Students are expected to use api and basics, and should stay out
-of core.
-"""
